b7db8d5e40724b68430c822067f36750dc8ed205
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-02-16 14:11:16 -0500

    Remove .pyc from repo. Updates feedback from script.

--- fabfile.py
+++ fabfile.py
@@ -1,39 +1,46 @@
 import csv
-from glob import glob
+import glob
 import json
 import os
 
-from bs4 import BeautifulSoup
+import bs4
 from fabric.api import *
 import requests
 
 def scrape_pdf():
     """
-    Scrape the crime PDFs from the campus police.
-    TODO: Ignore the file if we have a copy.
+    Scrape crime PDFs from the UO police Web site.
     """
+    print "Scrape: Crime index page"
     url = "http://police.uoregon.edu/content/campus-daily-crime-log"
     r = requests.get(url)
 
     if r.status_code == 200:
-
-        soup = BeautifulSoup(r.content)
+        soup = bs4.BeautifulSoup(r.content)
         links = soup.select('#attachments a')[1:]
 
+        print "Scrape: Evaluating %s links" % len(links)
         for link in links:
             link_url = link['href']
             file_name = link_url.split('http://police.uoregon.edu/sites/police.uoregon.edu/files/Clery Crime Log')[1].strip()
 
-            r = requests.get(link_url)
+            if not os.path.exists('data/pdf/%s' % file_name):
+                r = requests.get(link_url)
+                print 'Downloading %s' % file_name
 
-            if r.status_code == 200:
+                if r.status_code == 200:
+                    with open('data/pdf/%s' % file_name, 'wb') as writefile:
+                        writefile.write(r.content)
 
-                with open('data/%s' % file_name, 'wb') as writefile:
-                    writefile.write(r.content)
+                else:
+                    print "Failed downloading %s" % file_name
+
+    else:
+        print "Scrape: Crime index page error"
 
 def pdf_to_csv():
     """
-    Convert the crime PDFs to CSV for parsing.
+    Convert crime PDFs to CSV for parsing.
     This is really slow and will take a long time for each PDF, on the order of a minute or two.
 
     Requires pdftohtml to be installed in a directory that this user can access.
@@ -41,63 +48,70 @@ def pdf_to_csv():
 
     This project installs pdftable in the requirements.
     """
-    for path in glob('data/pdf/*.pdf'):
-
+    print "Convert: Evaluating %s files" % len(glob.glob('data/pdf/*.pdf'))
+    for path in glob.glob('data/pdf/*.pdf'):
         name, extension = os.path.splitext(path.replace('data/pdf/', ''))
-        os.system('pdftohtml -xml -stdout data/pdf/%s.pdf | pdftable -f data/csv/%s.csv' % (name, name))
 
-        print '+ %s' % name
+        if not os.path.exists('data/csv/%s.csv' % name):
+            print 'Converting %s' % name
+            os.system('pdftohtml -xml -stdout data/pdf/%s.pdf | pdftable -f data/csv/%s.csv' % (name, name))
 
 def clean_csv():
     """
+    Remove junk rows from generated CSVs.
     Many rows of the resulting CSV have junk data.
-    Clean these by ignoring any row missing a value in the first column.
-    Writes the result to a clean/ directory.
+    Ignore any row missing a value in the first column.
+    Write the result to a clean/ directory.
     """
-    for path in glob('data/csv/*.csv'):
-
-        rows = []
-
-        with open(path, 'rb') as readfile:
-
-            for row in csv.reader(readfile):
-                if row[0] != '':
-                    rows.append(row)
-
+    print "Clean: Evaluating %s files" % len(glob.glob('data/csv/*.csv'))
+    for path in glob.glob('data/csv/*.csv'):
         name, extension = os.path.splitext(path.replace('data/csv/', ''))
 
-        with open('data/csv/clean/%s%s' % (name, extension), 'wb') as writefile:
-            csvfile = csv.writer(writefile)
+        if not os.path.exists('data/csv/clean/%s%s' % (name, extension)):
+            print "Cleaning %s" % name
 
-            for row in rows:
-                row_list = []
+            rows = []
 
-                for item in row:
+            with open(path, 'rb') as readfile:
+                for row in csv.reader(readfile):
+                    if row[0] != '':
+                        rows.append(row)
+
+            with open('data/csv/clean/%s%s' % (name, extension), 'wb') as writefile:
+                csvfile = csv.writer(writefile)
+
+                for row in rows:
+                    row_list = []
+
                     # These CSVs were saved in Windows, and they're encoded Latin-1.
                     # Decode them and then re-encode them as UTF-8 for sanity.
-                    row_list.append(item.decode('latin-1').encode('utf-8'))
+                    [row_list.append(item.decode('latin-1').encode('utf-8')) for item in row]
 
-                csvfile.writerow(row_list)
+                    csvfile.writerow(row_list)
 
-def parse_csv():
+def parse_clean_csv():
     """
-    Finally, parse all of these CSVs into a big JSON file.
+    Parse CSV rows to dictionaries and add to a JSON file.
     """
     incidents = []
 
-    for path in glob('data/csv/clean/*.csv'):
+    print "Parse: Parsing %s cleaned CSV files" % len(glob.glob('data/csv/clean/*.csv'))
+
+    for path in glob.glob('data/csv/clean/*.csv'):
         name, extension = os.path.splitext(path.replace('data/csv/clean/', ''))
 
         with open(path, 'rb') as readfile:
 
             for incident in csv.DictReader(readfile):
+
+                # List which file we found this incident in, for troubleshooting and recordkeeping.
                 incident['originating_file'] = name
                 incidents.append(incident)
 
     with open('data/incidents.json', 'wb') as writefile:
         writefile.write(json.dumps(incidents))
 
-    print "%s incidents written to JSON" % len(incidents)
+    print "Parse: Writing %s incidents to JSON" % len(incidents)
 
 def parse_json():
     """
@@ -108,3 +122,12 @@ def parse_json():
 
     for incident in incident_list[2000:2050]:
         print incident
+
+def bootstrap_data():
+    """
+    Bootstrap this project by downloading PDFs, convert them to CSV, and parse the CSVs to JSON.
+    """
+    scrape_pdf()
+    pdf_to_csv()
+    clean_csv()
+    parse_clean_csv()
